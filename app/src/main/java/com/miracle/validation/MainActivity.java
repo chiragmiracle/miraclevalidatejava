2a0b05652cebc29b3ce3eaf2983ac4b61ce38ac2
chirag miracle <158419139+chiragmiracle@users.noreply.github.com>
2024-02-22 10:35:30 +0530

    create project
    create project

---
+++ app/src/main/java/com/miracle/validation/MainActivity.java
@@ -0,0 +1,19 @@
+package com.miracle.validation;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
